4b6205b1b70d122cb2d17df31e61543807dbae7a
roman_kononov <romankononovdev@mail.ru>
2021-04-28 10:49:15 +0700

    added section blog-one and responsiveness to header

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,39 @@
 $(function () {
-	$('.header__btn').on('click', function () {
-		$('.rightside-menu').removeClass('rightside-menu--close');
-	});
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
+  });
 
-	$('.rightside-menu--close').on('click', function () {
-		$('.rightside-menu').addClass('rightside-menu--close');
-	});
+  $(".rightside-menu--close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
 
-	$('.top__slider').slick({
-		dots: true,
-		arrows: false,
-		fade: true,
-		autoplay: true,
-	});
+  $(".header__btn-menu").on("click", function () {
+    $(".menu").toggleClass("menu--open");
+  });
 
-	$('.contact-slider').slick({
-		dots: true,
-		slidesToShow: 10,
-		slidesToScroll: 10,
-		arrows: false,
-	});
 
-	var mixer = mixitup('.gallery__inner', {
-		load: {
-			filter: '.living',
-		}
-	});
+  $(".top__slider").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+  });
+
+  $(".contact-slider").slick({
+    dots: true,
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    arrows: false,
+  });
+
+	$(".article-slider__box").slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slider-left.svg" alt="arrow left"><button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slider-right.svg" alt="arrow right"></button>'
+  });
+
+  var mixer = mixitup(".gallery__inner", {
+    load: {
+      filter: ".living",
+    },
+  });
 });
